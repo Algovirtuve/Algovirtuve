1528849aa12080177fb34021edadea16744987ca
Linux123123 <me@linux123123.com>
2026-04-17 20:00:22 +0300

    feat: very nice icon

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,13 +3,15 @@ import AppLogoIcon from '@/components/app-logo-icon';
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
-            </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">
-                    AlgoVirtuvė
-                </span>
+            <div className="flex items-center">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-md">
+                    <AppLogoIcon className="size-15 text-white" />
+                </div>
+                <div className="ml-1 grid flex-1 text-left text-sm">
+                    <span className="truncate text-xl font-semibold">
+                        AlgoVirtuvė
+                    </span>
+                </div>
             </div>
         </>
     );
