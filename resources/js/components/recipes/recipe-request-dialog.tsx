8826a7510b741ab9faeb59acfbe206c05874ee62
yaad33va <denisa.valinciute@ktu.edu>
2026-05-17 17:25:05 +0300

    lints

--- resources/js/components/recipes/recipe-request-dialog.tsx
+++ resources/js/components/recipes/recipe-request-dialog.tsx
@@ -1,5 +1,5 @@
 import { useForm } from '@inertiajs/react';
-import type {FormEvent} from 'react';
+import type { FormEvent } from 'react';
 import InputError from '@/components/input-error';
 import type { EnumOption } from '@/components/recipes/types';
 import { Button } from '@/components/ui/button';
